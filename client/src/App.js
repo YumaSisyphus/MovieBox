08ea9e3026803393d122fac5d56df46c3c451e8d
leartde <leartdell@gmail.com>
2023-05-21 19:06:04 +0200

    fixes

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/navbar';
+import { Router } from 'react-router-dom';
+import Movie from './components/movie';
+
+
+function App() {
+  return (
+    
+    
+<div className="App">
+    
+      {/* <Navbar/> */}
+      <Movie/>
+      
+
+    </div>
+
+
+    
+  );
+}
+
+export default App;
---
+++ client/src/components/navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <ul>
+                <li>Home</li>
+                <li>Log in</li>
+                <li>Create Account</li>
+                <li>Movies</li>
+                
+            </ul>
+        </div>
+      );
+}
+ 
+export default Navbar;
